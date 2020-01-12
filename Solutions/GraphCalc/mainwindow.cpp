74053eaee0ff7cc6fe1bd76be98c2c483baafc28
Sambeg Shrestha <sthasambeg2@outlook.com>
2020-01-12 18:23:34 +0545

    Experimental Graph Calc (NOT-INCLUDED)
    Experimentation for Graph calc (NOT INCLUDED IN FINAL PRODUCT)

---
+++ Solutions/GraphCalc/mainwindow.cpp
@@ -0,0 +1,37 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QtCore"
+#include "QtGui"
+#include "QDebug"
+#include "QMessageBox"
+#include <QVector>
+#include <cmath>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    customPlot = new QCustomPlot(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::on_pushButtonSwap_clicked()
+{
+    // generate some data:
+    QVector<double> x(101), y(101);
+    for (int i=0; i<101; ++i) {
+      x[i] = i/50.0 - 1; // x goes from -1 to 1
+      y[i] = x[i]*x[i]; // let's plot a quadratic function
+    }
+    this->customPlot->addGraph();
+    this->customPlot->graph(0)->setData(x, y);
+    this->customPlot->xAxis->setLabel("x");
+    this->customPlot->yAxis->setLabel("y");
+    this->customPlot->xAxis->setRange(-1, 1);
+    this->customPlot->yAxis->setRange(0, 1);
+    this->customPlot->replot();
+}
---
+++ Solutions/GraphCalc/mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QtCore>
+#include <QtGui>
+#include "Dependencies/Library/qcustomplot.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_pushButtonSwap_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QCustomPlot *customPlot;
+};
+
+#endif // MAINWINDOW_H
